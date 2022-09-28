b0c8e38b6d9a8219208fc2931cc0b8fe6c7af111
admin <admin@TeamCity>
2022-09-28 14:38:38 +0000

    TeamCity change in 'Python / StockWatch' project: parameters of 'Build' build configuration were updated

--- .teamcity/patches/buildTypes/Build.kts
+++ .teamcity/patches/buildTypes/Build.kts
@@ -11,6 +11,9 @@ accordingly, and delete the patch script.
 changeBuildType(RelativeId("Build")) {
     params {
         add {
+            password("system.refresh_token", "credentialsJSON:f1562ed9-33d4-4c7a-9427-ddcc9c23551d", label = "Refresh token", display = ParameterDisplay.HIDDEN)
+        }
+        add {
             password("system.access_token", "credentialsJSON:1c3c98ca-392a-40ee-87d9-a118b4fa071a", label = "Access Token", display = ParameterDisplay.HIDDEN, readOnly = true)
         }
     }
