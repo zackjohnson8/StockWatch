4e26dd55305aed1eba6db525357d527650b49e62
locallinux <zackjohnson8@gmail.com>
2022-11-25 13:03:01 -0800

    Refactored(message_bus): Improved readability

--- src/stock_watch/message_bus/message_bus.py
+++ src/stock_watch/message_bus/message_bus.py
@@ -21,8 +21,23 @@ class MessageBus(object):
         self._subscriptions.append({message_type: callback})
 
     def publish(self, message_type: MessageTypes, message: Any):
+        self._send_message_to_subscribers(message_type, message)
+
+    def _send_message_to_subscribers(self, message_type: MessageTypes, message: Any):
         for subscription in self._subscriptions:
-            subscription_type = list(subscription.keys())[0]
+            if self._is_subscribed_to_message_type(subscription=subscription, message_type=message_type):
+                self._call_subscription_callback(subscription=subscription, message=message)
+
+    def _is_subscribed_to_message_type(self, subscription: {}, message_type: MessageTypes):
+        for subscription in self._subscriptions:
+            subscription_type = self._get_subscription_type(subscription)
             if subscription_type.value == message_type.value:
-                call_back = subscription[subscription_type]
-                call_back(message)
+                return True
+        return False
+
+    def _get_subscription_type(self, subscription: {}):
+        return list(subscription.keys())[0]
+
+    def _call_subscription_callback(self, subscription: {}, message: Any):
+        callback = list(subscription.values())[0]
+        callback(message)
