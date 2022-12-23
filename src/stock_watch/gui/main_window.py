f11ca67dfabc0dd40a6314b67ec92029a4c0cb77
locallinux <zackjohnson8@gmail.com>
2022-12-23 09:53:37 -0800

    - Rename window - Size window to fit centered and half size on monitor

--- src/stock_watch/gui/main_window.py
+++ src/stock_watch/gui/main_window.py
@@ -1,8 +1,17 @@
+from screeninfo import get_monitors
 from PyQt6.QtWidgets import QMainWindow
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Hello World")
+        self.setup_window_geometry()
+        self.setWindowTitle("Stock Watch")
         self.show()
+
+    def setup_window_geometry(self):
+        # Get the primary monitor
+        monitor = get_monitors()[0]
+        # Set the geometry of the window to the monitor's center
+        self.setGeometry(int(monitor.width/4), int(monitor.height/4), int(monitor.width/2), int(monitor.height/2))
