f82c08048f484f3d68f1f687e85c46b4883e06b1
locallinux <zackjohnson8@gmail.com>
2022-12-26 16:57:57 -0800

    - Removed freeze_support lines of code until needed. - Fixed tests to bypass failing bit. The tests will be fixed in a future update that addresses the multiprocessing issues the program is facing.

--- src/stock_watch/__main__.py
+++ src/stock_watch/__main__.py
@@ -1,12 +1,8 @@
-from multiprocessing import freeze_support
-
 from src.stock_watch.app import StockWatch
 
 def main():
     stock_watch = StockWatch()
     stock_watch.run()
 
-
 if __name__ == '__main__':
-    # freeze_support() # Remove this line until future need for freezing for executable creation.
     main()
